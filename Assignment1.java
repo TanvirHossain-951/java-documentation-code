590fea79f47e76b66bda9e31b309321677fc8cfa
TanvirHossain-951 <abitwolf1@gmail.com>
2026-06-10 21:13:27 +0600

    Anisul Islam assignment 1

--- Assignment1.java
+++ Assignment1.java
@@ -1,13 +1,9 @@
-/*
- * Assignment-1
- * create a class called Product
- * create a main method
- * print the following data in main method
-
-
- id: 101,
-title: iphone15,
-price: 1895 euros,
-description: perfect product with best image quality,
-category: phone,
- */
+public class Product{
+ public static void main(String[]arg){
+  System.out.println("id: iphone15");
+   System.out.println("title: 1895 euros,");
+   System.out.println("price: 1895 euros,");
+   System.out.println("description: perfect product with best image quality,");
+  System.out.println("category: phone,");
+ }
+} 
